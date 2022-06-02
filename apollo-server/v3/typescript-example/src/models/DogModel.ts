d1c8394e1e7dc72bfab84c52960b66a288583360
Rose Koron <rkoron007@gmail.com>
2022-06-02 14:27:13 -0700

    Remove extra files

--- apollo-server/v3/typescript-example/src/models/DogModel.ts
+++
@@ -1,8 +0,0 @@
-// You can tell GraphQL Code Generator to make types based on your custom model types.
-export type DogModel = {
-  id: string;
-  name: string;
-  breed: string;
-  age: number;
-  image: string;
-};
