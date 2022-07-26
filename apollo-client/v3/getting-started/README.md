dbb2447e303b00813d3077e229d7fbc87d9365df
Rose M Koron <32436232+rkoron007@users.noreply.github.com>
2022-07-26 14:55:20 -0700

    Add React client "getting started" code  (#45)

---
+++ apollo-client/v3/getting-started/README.md
@@ -0,0 +1,16 @@
+# Get started with Apollo Client
+
+This is the complete code from [Get started with Apollo Client](https://www.apollographql.com/docs/react/get-started).
+
+## Run locally
+
+```shell
+npm install
+npm start
+```
+
+## Run in CodeSandbox
+
+<a href="https://codesandbox.io/s/github/apollographql/docs-examples/tree/main/apollo-client/v3/getting-started?fontsize=14&hidenavigation=1&theme=dark">
+  <img alt="Edit client-getting-started" src="https://codesandbox.io/static/img/play-codesandbox.svg">
+</a>
---
+++ apollo-client/v3/getting-started/src/App.js
@@ -0,0 +1,44 @@
+import { useQuery, gql } from '@apollo/client';
+
+const GET_LOCATIONS = gql`
+  query getLocations {
+    locations {
+      id
+      name
+      description
+      photo
+    }
+  }
+`;
+
+function DisplayLocations() {
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  return data.locations.map(({ id, name, description, photo }) => (
+    <div key={id}>
+      <h3>{name}</h3>
+      <img width="400" height="250" alt="location-reference" src={`${photo}`} />
+      <br />
+      <b>About this location:</b>
+      <p>{description}</p>
+      <br />
+    </div>
+  ));
+}
+
+export default function App() {
+  return (
+    <div>
+      <h2>
+        My first Apollo app
+        <span role="img" aria-label="rocket">
+          🚀
+        </span>
+      </h2>
+      <br />
+      <DisplayLocations />
+    </div>
+  );
+}
