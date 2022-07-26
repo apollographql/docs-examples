1659dd55ef36d50e4d3d5698c7c31c21b8b4f16f
Rose Koron <rkoron007@gmail.com>
2022-07-26 12:19:43 -0700

    Add Apollo client get started demo

---
+++ apollo-client/v3/getting-started/src/index.js
@@ -0,0 +1,17 @@
+import { React } from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import App from './App';
+
+const client = new ApolloClient({
+  uri: 'https://flyby-gateway.herokuapp.com/',
+  cache: new InMemoryCache(),
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+);
