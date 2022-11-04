2a99b2cf24976cec4dcab8a1247c02f6df884268
Rose M Koron <32436232+rkoron007@users.noreply.github.com>
2022-11-04 14:32:54 -0700

    Add ESM support for JEST tests (#56)

--- apollo-server/v4/integration-testing/jest.config.cjs
+++ apollo-server/v4/integration-testing/jest.config.cjs
@@ -2,11 +2,14 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  testPathIgnorePatterns: ['.js'],
   globals: {
     // when we are testing we want to use a slightly different config
     // to allow for jest types
     'ts-jest': {
       tsconfig: '<rootDir>/src/__tests__/tsconfig.json',
+      useESM: true,
     },
   },
 };
