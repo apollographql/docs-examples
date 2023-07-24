2689510021408aab49cd4f651ed3472b43fef59f
alessia <alessia@apollographql.com>
2023-07-24 15:17:34 -0400

    chore: prettier fixes

--- apollo-client/v3/getting-started/src/index.js
+++ apollo-client/v3/getting-started/src/index.js
@@ -1,17 +1,17 @@
-import { React } from 'react';
-import * as ReactDOM from 'react-dom/client';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import App from './App';
+import { React } from "react";
+import * as ReactDOM from "react-dom/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import App from "./App";
 
 const client = new ApolloClient({
-  uri: 'https://flyby-router-demo.herokuapp.com/',
+  uri: "https://flyby-router-demo.herokuapp.com/",
   cache: new InMemoryCache(),
 });
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <ApolloProvider client={client}>
     <App />
-  </ApolloProvider>,
+  </ApolloProvider>
 );
